930ca939c206e647dc16d08df737e49c912a08ef
donRumata <donrumata03@gmail.com>
2022-10-25 15:40:18 +0300

    Solve: write proc macro for converting enum discriminant into enum value

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,38 @@
+use quote::quote;
+use proc_macro2::TokenStream;
+use syn::{Data, DataEnum, Variant};
+
 #[proc_macro_derive(FromPrimitive)]
 pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    let _input = syn::parse_macro_input!(input as syn::DeriveInput);
+	let _input = syn::parse_macro_input!(input as syn::DeriveInput);
+	let syn::DeriveInput {
+		attrs, vis, ident: enum_ident, generics, data
+	} = _input;
 
-    unimplemented!()
+	let match_arms: proc_macro2::TokenStream = if let Data::Enum(DataEnum {
+		                  enum_token,
+		                  brace_token,
+		                  variants
+	                  }) = data {
+		variants.into_iter()
+			.enumerate()
+			.map(|(i, Variant{attrs, ident, fields, discriminant})|
+				quote! {#i => Ok(#enum_ident::#ident),})
+			.collect()
+	} else {panic!()};
+
+
+	quote! {
+	    impl TryFrom<usize> for #enum_ident {
+		    type Error = ();
+
+		    fn try_from(value: usize) -> Result<Self, Self::Error> {
+		        match value {
+					#match_arms
+		            _ => Err(())
+		        }
+		    }
+		}
+    }.into()
 }
+
--- tests/integration.rs
+++ tests/integration.rs
@@ -3,10 +3,10 @@ use from_primitive::FromPrimitive;
 #[derive(Debug, PartialEq)]
 #[derive(FromPrimitive)]
 enum Example {
-    A,
-    B,
-    C,
-    D
+	A,
+	B,
+	C,
+	D
 }
 
 // Следующий код должен быть сгенерирован макросом:
@@ -29,12 +29,12 @@ impl TryFrom<usize> for Example {
 // Тест для проверки:
 #[test]
 fn test() {
-    assert_eq!(Example::A as usize, 0);
-    assert_eq!(Example::B as usize, 1);
-    assert_eq!(Example::C as usize, 2);
-    assert_eq!(Example::D as usize, 3);
-    for (i, val) in (0..4).zip([Example::A, Example::B, Example::C, Example::D].into_iter()) {
-        assert_eq!(Example::try_from(i), Ok(val));
-    }
-    assert!(Example::try_from(100).is_err());
+	assert_eq!(Example::A as usize, 0);
+	assert_eq!(Example::B as usize, 1);
+	assert_eq!(Example::C as usize, 2);
+	assert_eq!(Example::D as usize, 3);
+	for (i, val) in (0..4).zip([Example::A, Example::B, Example::C, Example::D].into_iter()) {
+		assert_eq!(Example::try_from(i), Ok(val));
+	}
+	assert!(Example::try_from(100).is_err());
 }
